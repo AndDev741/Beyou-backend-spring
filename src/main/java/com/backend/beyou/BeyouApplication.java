17d732ef8d256db05c3ef1d22c06bb8abe5d40fd
AndDev <anddesenvolved@gmail.com>
2024-06-27 13:46:20 -0300

    First commit

---
+++ src/main/java/com/backend/beyou/BeyouApplication.java
@@ -0,0 +1,13 @@
+package com.backend.beyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyouApplication.class, args);
+	}
+
+}
