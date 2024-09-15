25d4413a1f2b2e070c6ac9a8ac80fa61357ae558
andDev741 <anddesenvolved@gmail.com>
2024-09-15 09:32:27 -0300

    feat: Spring boot initial configuration

---
+++ src/test/java/beyou/beyouapp/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package beyou.beyouapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
