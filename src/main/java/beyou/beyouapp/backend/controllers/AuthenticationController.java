3ef4444d773bfd986acb53c0fd6709d1d4715d2f
andDev741 <anddesenvolved@gmail.com>
2024-09-25 15:00:05 -0300

    feat: Authentication Controller created with login and register endpoints

---
+++ src/main/java/beyou/beyouapp/backend/controllers/AuthenticationController.java
@@ -0,0 +1,56 @@
+package beyou.beyouapp.backend.controllers;
+
+import beyou.beyouapp.backend.user.UserService;
+import beyou.beyouapp.backend.user.dto.UserLoginDTO;
+import beyou.beyouapp.backend.user.dto.UserRegisterDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @Autowired
+    private UserService userService;
+
+    //Documentation
+    @Operation(summary = "Realize the user login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User logged successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Map.class))}),
+            @ApiResponse(responseCode = "401", description = "Email or password incorrect",
+                    content = @Content)
+    })
+    //
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> doLogin(HttpServletResponse response, @RequestBody @Valid UserLoginDTO userLoginDTO){
+        return userService.doLogin(response, userLoginDTO);
+    }
+
+    //Documentation
+    @Operation(summary = "Realize the user register")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User registered successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Map.class))}),
+            @ApiResponse(responseCode = "401", description = "Email already in use",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid or missed credentials",
+                    content = @Content)
+    })
+    //
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, String>> doRegister(@RequestBody @Valid UserRegisterDTO userRegisterDTO){
+        return userService.registerUser(userRegisterDTO);
+    }
+}
