6cb6216ea1f65e29ac02819af4267a3ce8d00442
andDev741 <anddesenvolved@gmail.com>
2024-10-16 11:06:54 -0300

    feat: Category service created with crud methods

---
+++ src/main/java/beyou/beyouapp/backend/domain/category/CategoryService.java
@@ -0,0 +1,65 @@
+package beyou.beyouapp.backend.domain.category;
+
+import beyou.beyouapp.backend.domain.category.dto.CategoryEditRequestDTO;
+import beyou.beyouapp.backend.domain.category.dto.CategoryRequestDTO;
+import beyou.beyouapp.backend.exceptions.category.CategoryNotFound;
+import beyou.beyouapp.backend.exceptions.user.UserNotFound;
+import beyou.beyouapp.backend.user.User;
+import beyou.beyouapp.backend.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Category getCategory(String categoryId){
+        return categoryRepository.findById(UUID.fromString(categoryId))
+                .orElseThrow(() -> new CategoryNotFound("Category not found"));
+    }
+
+    public ArrayList<Category> getAllCategories(String userId){
+        return categoryRepository.findAllByUserId(UUID.fromString(userId))
+                .orElseThrow(() -> new UserNotFound("User not found"));
+    }
+
+    public ResponseEntity<Map<String, Object>> createCategory(CategoryRequestDTO categoryRequestDTO){
+        User user = userRepository.findById(UUID.fromString(categoryRequestDTO.userId()))
+                .orElseThrow(() -> new UserNotFound("User not found"));
+        Category newCategory = new Category(categoryRequestDTO, user);
+        categoryRepository.save(newCategory);
+
+        return ResponseEntity.ok().body(Map.of("success", newCategory));
+    }
+
+    public ResponseEntity<Map<String, Object>> editCategory(CategoryEditRequestDTO categoryEditRequestDTO){
+        Category categoryToEdit = getCategory(categoryEditRequestDTO.categoryId());
+
+        categoryToEdit.setName(categoryEditRequestDTO.name());
+        categoryToEdit.setDescription(categoryEditRequestDTO.description());
+        categoryToEdit.setIconId(categoryEditRequestDTO.icon());
+
+        categoryRepository.save(categoryToEdit);
+
+        return ResponseEntity.ok().body(Map.of("success", categoryToEdit));
+    }
+
+    public ResponseEntity<Map<String, String>> deleteCategory(String categoryId){
+        try{
+            Category category = categoryRepository.findById(UUID.fromString(categoryId))
+                    .orElseThrow(() -> new CategoryNotFound("Category not found"));
+
+            categoryRepository.delete(category);
+            return ResponseEntity.ok().body(Map.of("success", "Category deleted successfully"));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(Map.of("error", "errorTryingToDeleteCategory"));
+        }
+    }
+}
