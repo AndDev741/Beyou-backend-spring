31f144140cc731c22c7012d77e48298f578fb761
AndDev741 <anddesenvolved@gmail.com>
2025-02-08 09:32:48 -0300

    refactor: Added the annotation @SupressWarnings to the filter method

--- src/main/java/beyou/beyouapp/backend/security/SecurityFilter.java
+++ src/main/java/beyou/beyouapp/backend/security/SecurityFilter.java
@@ -27,6 +27,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     UserRepository userRepository;
 
+    @SuppressWarnings("null")
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
